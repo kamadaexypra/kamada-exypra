e53d307b2500c5482e9257943faeced46b2118a7
kamadaexypra <sambaelputoamo@gmail.com>
2026-08-27 22:44:07 +0200

    Add function to toggle menu visibility

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const botonTema = document.getElementById("boton-tema");
 botonTema.addEventListener("click", function () {
     document.body.classList.toggle("modo-claro");
 });
+function abrirMenu() {
+
+    const menu = document.getElementById("menuKamada");
+
+    menu.classList.toggle("mostrar");
+
+}
