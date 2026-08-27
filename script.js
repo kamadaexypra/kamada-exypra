c331a8062365015a2a17a308dab50366c921d2ab
kamadaexypra <sambaelputoamo@gmail.com>
2026-08-27 22:56:54 +0200

    Implement dark and light mode functions
    Added functions for dark and light mode toggling.

--- script.js
+++ script.js
@@ -8,5 +8,13 @@ function abrirMenu() {
     const menu = document.getElementById("menuKamada");
 
     menu.classList.toggle("mostrar");
+function modoOscuro() {
+    document.body.classList.remove("modo-claro");
+    document.body.classList.add("modo-oscuro");
+}
 
+function modoClaro() {
+    document.body.classList.remove("modo-oscuro");
+    document.body.classList.add("modo-claro");
+}
 }
